41c8a4d5438014c2eadd078069ed8aceaf05d9fc
cantoken <cantoken@yandemail.com>
2021-05-12 23:44:56 +0300

    55

--- src/App.tsx
+++ src/App.tsx
@@ -54,9 +54,9 @@ const App: React.FC = () => {
             {/* <Route path="/pools"> */}
             {/*  <Pools /> */}
             {/* </Route> */}
-            <Route path="/lottery"> 
-              <Lottery />
-            </Route> 
+            {/* <Route path="/lottery"> */}
+            {/*  <Lottery /> */}
+            {/* </Route> */}
             {/* <Route path="/ifo"> */}
             {/*  <Ifos /> */}
             {/* </Route> */}
