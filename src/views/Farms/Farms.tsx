628b1b01f8a2896e322aeb377e8418e2e855b780
yogurtfinance <80599601+yogurtfinance@users.noreply.github.com>
2021-05-15 23:36:50 +0300

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -84,7 +84,7 @@ const Farms: React.FC<FarmsProps> = (farmsProps) => {
         const cakeRewardPerMonth = cakeRewardPerYear.div(12)
 
         let apy = cakePrice.times(cakeRewardPerYear);
-        if (farm.quoteTokenSymbol === QuoteToken.bctrm) {
+        if (farm.quoteTokenSymbol === QuoteToken.bacterium) {
           apy = cakePrice.times(cakeRewardPerMonth);
         }
         let totalValue = new BigNumber(farm.lpTotalInQuoteToken || 0);
